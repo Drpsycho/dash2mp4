1707615b768e35023e0c258520760f3d4c195b47
drpsycho <drpsychoz@gmail.com>
2015-08-30 16:10:05 +0600

    refatoring - moved some variables to the one structure

--- pdm4.go
+++ pdm4.go
@@ -13,6 +13,10 @@ import (
 
 type ByLength []string
 
+type InputParam struct {
+	outputname, pathtofolder, inputname string
+}
+
 // regexp for detect files and sort it
 // example name file: samefilename_X_XXX.mp4
 var digitsRegexp = regexp.MustCompile(`(\w+?)_(\d)_(\d+)\.mp4`)
@@ -46,7 +50,7 @@ func (s ByLength) Less(i, j int) bool {
 	return res_i_in_digit < res_j_in_digit
 }
 
-func clearfile(filename string) {
+func outputfileremove(filename string) {
 	os.Remove(filename)
 }
 
@@ -74,55 +78,8 @@ func packmp4(file string, outputfilename string) {
 	fmt.Printf("Copied %v bytes\n", n)
 }
 
-func findfiles(files []os.FileInfo, outputfilename string, inputname string) {
-	var filelist ByLength
-
-	for _, file := range files {
-		if !(file.Mode().IsRegular()) {
-			continue
-		}
-
-		if !(filepath.Ext(file.Name()) == ".mp4") {
-			continue
-		}
-
-		if file.Name() == outputfilename {
-			continue
-		}
-
-		matched, _ := filepath.Match(inputname+"*", file.Name())
-		if !(matched) {
-			continue
-		}
-
-		res := firstfile.MatchString(file.Name())
-		if res {
-			packmp4(file.Name(), outputfilename)
-		} else {
-			filelist = append(filelist, file.Name())
-		}
-
-	}
-
-	sort.Sort(filelist)
-
-	for _, filename := range filelist {
-		packmp4(filename, outputfilename)
-	}
-}
-
-func main() {
-
-	outputname := flag.String("o", "out.mp4", "Name for output file")
-	pathtofolder := flag.String("p", "."+string(filepath.Separator), "Path to work folder")
-	inputname := flag.String("i", "All", "Base name for input files")
-	flag.Parse()
-
-	if *inputname == "All" {
-		*inputname = ""
-	}
-
-	d, err := os.Open(*pathtofolder)
+func openfolder(pathtofolder string) []os.FileInfo {
+	d, err := os.Open(pathtofolder)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -134,7 +91,67 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	return files
+}
 
-	clearfile(*outputname)
-	findfiles(files, *outputname, *inputname)
+func findfiles(param InputParam) {
+
+	files := openfolder(param.pathtofolder)
+
+	var filelist ByLength
+
+	for _, file := range files {
+		if !(file.Mode().IsRegular()) {
+			continue
+		}
+
+		if !(filepath.Ext(file.Name()) == ".mp4") {
+			continue
+		}
+
+		if file.Name() == param.outputname {
+			continue
+		}
+
+		matched, _ := filepath.Match(param.inputname+"*", file.Name())
+		if !(matched) {
+			continue
+		}
+
+		res := firstfile.MatchString(file.Name())
+		if res {
+			packmp4(file.Name(), param.outputname)
+		} else {
+			filelist = append(filelist, file.Name())
+		}
+
+	}
+
+	sort.Sort(filelist)
+
+	for _, filename := range filelist {
+		if digitsRegexp.MatchString(filename) {
+			packmp4(filename, param.outputname)
+		}
+	}
+}
+
+func parseInputParam() InputParam {
+	var param InputParam
+	param.outputname = *(flag.String("o", "out.mp4", "Name for output file"))
+	param.pathtofolder = *(flag.String("p", "."+string(filepath.Separator), "Path to work folder"))
+	param.inputname = *(flag.String("i", "All", "Base name for input files"))
+	flag.Parse()
+
+	if param.inputname == "All" {
+		param.inputname = ""
+	}
+
+	outputfileremove(param.outputname)
+
+	return param
+}
+
+func main() {
+	findfiles(parseInputParam())
 }
